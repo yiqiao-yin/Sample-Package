b0d91f787bc096e3db1bde05552abff3e5e1b249
Yiqiao Yin <32333117+yiqiao-yin@users.noreply.github.com>
2021-07-02 10:48:11 -0400

    Create sampleFunction.R

---
+++ sampleFunction.R
@@ -0,0 +1,12 @@
+#' @title Some Function
+#' @description This is a teaching exercise.
+#' @param symbol
+#' @return NULL
+#' @examples  sampleFunction()
+#' @export sampleFunction
+#'
+#' # Define function
+sampleFunction <- function(a, b) {
+  c = a + b
+  return(c)
+} # End
